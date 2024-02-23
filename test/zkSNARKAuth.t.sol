724fc6b053a2eb202948ec6052ba140dc63696af
Sudipta Basak <sudiptab2100@gmail.com>
2024-02-23 19:44:01 +0530

    Renamed Foundry default files

--- test/zkSNARKAuth.t.sol
+++ test/zkSNARKAuth.t.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {zkSNARKAuth} from "../src/zkSNARKAuth.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract zkSNARKAuthTest is Test {
+    zkSNARKAuth public counter;
 
     function setUp() public {
-        counter = new Counter();
+        counter = new zkSNARKAuth();
         counter.setNumber(0);
     }
 
