56292a74091d9b214dcd39386f6d11dd86e6e75d
Sudipta Basak <sudiptab2100@gmail.com>
2024-02-25 04:14:51 +0530

    Generate Hash Data json file

--- hash_generator.py
+++ hash_generator.py
@@ -1,5 +1,6 @@
 import hashlib
 import binascii
+import json
 
 
 def hash_gen(a: int, b: int, c: int, k: int):
@@ -24,11 +25,25 @@ def hash_gen(a: int, b: int, c: int, k: int):
 def zokIp(password: str, count: int):
     bin_pass = format(int(binascii.hexlify(bytes(password, "utf-8")), 16), "#0386b")[2: ]
     a, b, c, k = int(bin_pass[: 32 * 4], 2), int(bin_pass[32 * 4: 64 * 4], 2), int(bin_pass[64 * 4: ], 2), count
+    
+    op = dict()
+    op["password"] = password
+    op["a"] = a
+    op["b"] = b
+    op["c"] = c
+    op["k"] = k
+    
     print(f"a: {a}\nb: {b}\nc: {c}\nk: {k}\n")
     v1, v2 = hash_gen(a, b, c, 0)
+    op["h"] = [v1, v2]
     print(f"sha256([a, b, c, 0]) =>\n  {v1}\n  {v2}\n")
+    
     v1, v2 = hash_gen(a, b, c, k)
     print(f"sha256([a, b, c, k]) =>\n  {v1}\n  {v2}\n")
+    op["hh"] = [v1, v2]
+    
+    with open("hash_data.json", "w") as f:
+        json.dump(op, f, indent=4)
     
 print("\nResults:\n")
 zokIp("password", 1)
