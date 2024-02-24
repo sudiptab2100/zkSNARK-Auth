6d038393f5d07f7b502f5cfd53a2ca46ef71e05f
Sudipta Basak <sudiptab2100@gmail.com>
2024-02-24 22:39:54 +0530

    Hash Generator Script Major Bug fix

--- hash_generator.py
+++ hash_generator.py
@@ -3,10 +3,10 @@ import binascii
 
 
 def hash_gen(a: int, b: int, c: int, k: int):
-    ba = format(a, "#034b")[2: ]
-    bb = format(b, "#034b")[2: ]
-    bc = format(c, "#034b")[2: ]
-    bk = format(k, "#034b")[2: ]
+    ba = format(a, "#034x")[2: ]
+    bb = format(b, "#034x")[2: ]
+    bc = format(c, "#034x")[2: ]
+    bk = format(k, "#034x")[2: ]
     
     bdata = ba + bb + bc + bk
     bdata_bytes = bytes.fromhex(bdata)
@@ -22,8 +22,8 @@ def hash_gen(a: int, b: int, c: int, k: int):
     return val1, val2
 
 def zokIp(password: str, count: int):
-    bin_pass = format(int(binascii.hexlify(bytes(password, "utf-8")), 16), "#098b")[2: ]
-    a, b, c, k = int(bin_pass[: 32], 2), int(bin_pass[32: 64], 2), int(bin_pass[64: ], 2), count
+    bin_pass = format(int(binascii.hexlify(bytes(password, "utf-8")), 16), "#0386b")[2: ]
+    a, b, c, k = int(bin_pass[: 32 * 4], 2), int(bin_pass[32 * 4: 64 * 4], 2), int(bin_pass[64 * 4: ], 2), count
     print(f"a: {a}\nb: {b}\nc: {c}\nk: {k}\n")
     v1, v2 = hash_gen(a, b, c, 0)
     print(f"sha256([a, b, c, 0]) =>\n  {v1}\n  {v2}\n")
